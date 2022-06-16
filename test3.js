f7020a8aed2dc51cc87c4d035ea3d89a0f7fd572
unknown <89188599154B@gmail.com>
2022-06-16 16:09:01 +0300

    newTest

---
+++ test3.js
@@ -0,0 +1,23 @@
+// 5 > 4                 true
+// "ананас" > "яблоко"   false
+// "2" > "12"            true
+// undefined == null     true
+// undefined === null    false
+// null == "\n0\n"       false
+// null === +"\n0\n"     false
+
+
+
+
+
+if (time == "утро") {
+    message = 'пей кофе и иди батрачить';
+  } else if (time == "обед") {
+    message = 'едем в сталовку ';
+  } else if (time == "вечер") {
+    message = 'идем домой';
+  } else {
+    message = 'спим';
+  }
+
+  console.log(time == "вечер")
